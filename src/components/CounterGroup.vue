fc6781fc42d50b9fc453f78e5d077797adb1596d
Tiger Zhu <tiger.zhu@oocl.com>
2019-07-19 18:29:03 +0800

    add a CounterGroup and finish the Vue Practice_2_Multi_Counter

---
+++ src/components/CounterGroup.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+    <input type="text" v-model.number="counteerNum">
+
+    <div v-for="index in counteerNum" v-bind:key="index">
+        <colculate @add-number="addTotal" @reduce-number="reduceTotal"></colculate>
+    </div>
+
+   <counter-sum :total="total"></counter-sum>
+
+</div>
+    
+</template>
+
+
+
+<script>
+import colculate from './colculate.vue'
+import CounterSum from './CounterSum.vue'
+export default {
+  name: 'counterGroup',
+  components:{
+      colculate,
+      CounterSum
+  },
+  data(){
+      return{
+          value:0,
+          counteerNum:0,
+          total:0
+      }
+  },
+  methods:{
+      addTotal:function(value){
+          this.total+=value
+      },
+      reduceTotal:function(value){
+          this.total-=value
+      }
+      
+  }
+}
+</script>

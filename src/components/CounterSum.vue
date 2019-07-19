23045d4f7f16954a357c9f4a785f74b46c285b84
Tiger Zhu <tiger.zhu@oocl.com>
2019-07-19 18:33:34 +0800

    add CounterSum in the Practice Multi Counter

---
+++ src/components/CounterSum.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    {{ total }}
+</div>
+</template>
+<script>
+export default {
+    name:'CounterSum',
+    props:['total'],
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
